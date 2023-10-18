6f8557cc9d8f79300a56ff856550459ddc9649ca
hifzurmax <hifzur.syl@gmail.com>
2023-10-19 01:38:50 +0600

    basic server setup with MongoDB

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//hifzursyl
+//n8kjeuplLm1sZaLj
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r5qnv1b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const brandCollection = client.db('BrandShopDB').collection('brands');
+    const productCollection = client.db('BrandShopDB').collection('products');
+
+
+    app.get('/brands', async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.get('/product/:brand', async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brand: brand }; 
+      const result = await productCollection.find(query).toArray(); 
+      res.send(result);
+    });
+
+
+    app.post('/product', async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Brand shop server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Brand shop Server is running on port: ${port}`)
+})
