a9e6ba272558186d4be51ad439ba2b2bf92c1dd6
hifzurmax <hifzur.syl@gmail.com>
2023-10-20 08:52:16 +0600

    Product update api created

--- index.js
+++ index.js
@@ -82,6 +82,28 @@ async function run() {
       res.send(result)
     })
 
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+
+      const product = {
+        $set: {
+          image: updatedProduct.image,
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          description: updatedProduct.description
+        }
+      }
+
+      const result = await productCollection.updateOne(filter, product, options);
+      res.send(result);
+    })
+
     app.delete('/cart/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
